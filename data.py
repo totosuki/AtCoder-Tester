fd1768aeb2a465f1825fec279bf910e0cb18c34a
totosuki <e231kari@gmail.com>
2023-07-25 17:56:53 +0900

    config.pyの表示とsetting.shの--helpのテキストを追加

--- data.py
+++ data.py
@@ -4,7 +4,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--contest", default = "abc", help = "Contest Name [default = abc]")
 parser.add_argument("number", help = "Contest number")
 parser.add_argument("problem", help = "Problem alphabet")
-parser.add_argument("-m", "--mode", default = "test", choices = ["test", "submit"], help = "Select mode")
+parser.add_argument("-m", "--mode", default = "test", choices = ["test", "submit"], help = "Select mode [default = test] [test : Sample case test only] [submit : Automatically submit when sample case is All AC]")
 
 args = parser.parse_args()
 contest = args.contest
