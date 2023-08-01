509542dbfe3341a962007b05174f3250cf2d28c0
totosuki <e231kari@gmail.com>
2023-08-01 14:41:49 +0900

    Bashスクリプト内のsumitコマンドに関する修正

--- data.py
+++ data.py
@@ -2,15 +2,15 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--contest", default = "abc", help = "Contest Name [default = abc]")
+parser.add_argument("mode", default = "test", choices = ["test", "submit"], help = "Select mode [default = test] [test : Sample case test only] [submit : Automatically submit when sample case is All AC]")
 parser.add_argument("number", help = "Contest number")
 parser.add_argument("problem", help = "Problem alphabet")
-parser.add_argument("-m", "--mode", default = "test", choices = ["test", "submit"], help = "Select mode [default = test] [test : Sample case test only] [submit : Automatically submit when sample case is All AC]")
 
 args = parser.parse_args()
 contest = args.contest
+mode = args.mode
 number = args.number
 problem = args.problem
-mode = args.mode
 
 LOGIN_URL = 'https://atcoder.jp/login'
 PROB_URL = "https://atcoder.jp/contests/{}{}/tasks/{}{}_{}".format(contest.lower(), number.zfill(3), contest.lower(), number.zfill(3), problem.lower())
